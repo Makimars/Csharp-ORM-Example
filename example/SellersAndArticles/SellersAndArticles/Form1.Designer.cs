69b83a300e46de8f53fb39e0fd991947d1a5da13
Barsonica <barsonica@gmail.com>
2020-01-02 17:42:20 +0100

    Restructure, adding exaple usage

---
+++ example/SellersAndArticles/SellersAndArticles/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SellersAndArticles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(48, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(491, 361);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(623, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Article";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(622, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Seller";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(623, 240);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DataRelation";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ example/SellersAndArticles/SellersAndArticles/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Csharp_ORM_Example;
+
+namespace SellersAndArticles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Repository.setConnString(Properties.Settings.Default.connString);
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.Text = "";
+
+            ArticlesRepository artRepo = ArticlesRepository.getInstance();
+
+            EntityList iterator = artRepo.getList();
+            /*
+            iterator.setLimit(3)
+                .setOrder("Id", Order.DESC);*/
+
+            Article[] entities = (Article[])iterator.fetch();
+
+            foreach (Article ar in entities)
+            {
+                richTextBox1.Text += ar.Name + " " + ar.Id + "\n";
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+
+            SellersRepository sellRepo = SellersRepository.getInstance();
+
+            EntityList sellIterator = sellRepo.getList();
+            sellIterator.setLimit(1); 
+                
+            Seller[] sellers = (Seller[])sellIterator.fetch();
+            
+            foreach (Seller ar in sellers)
+            {
+                ar.Name = "zmena";
+                ar.save();
+                richTextBox1.Text += ar.Name + " " + ar.Id + "\n";
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+
+            SellersRepository sellRepo = SellersRepository.getInstance();
+            ArticlesRepository artRepo = ArticlesRepository.getInstance();
+
+            EntityList sellIterator = sellRepo.getList();
+            EntityList artIterator = artRepo.getList();
+
+            DataRelationIterator dataRel = sellIterator.toDataRelationIterator();
+
+            dataRel.joinWith(artIterator, "Id", "seller_id");
+
+            DataTable dt = dataRel.fetch();
+            DataRowCollection rows = dt.Rows;
+            DataColumnCollection columns = dt.Columns;
+
+            for (int i = 0; i < columns.Count; i++)
+            {
+                DataColumn column = columns[i];
+
+                richTextBox1.Text += column.ToString() + " ";
+            }
+
+            richTextBox1.Text += "\n";
+
+            for (int i = 0; i < rows.Count; i++)
+            {
+                DataRow row = rows[i];
+
+                var items = row.ItemArray;
+
+                for (int a = 0; a < items.Length; a++)
+                {
+                    richTextBox1.Text += items[a].ToString() + " ";
+                }
+
+                richTextBox1.Text += "\n";
+            }
+
+        }
+    }
+}
