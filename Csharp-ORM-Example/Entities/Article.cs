3a55e668a041e3a0b51748a0665588212d94fb05
Barsonica <barsonica@gmail.com>
2020-01-02 14:11:47 +0100

    Null values working

--- Csharp-ORM-Example/Entities/Article.cs
+++ Csharp-ORM-Example/Entities/Article.cs
@@ -6,24 +6,24 @@ namespace Csharp_ORM_Example
     public class Article : Entity
     {
 
-        int seller_id;
-        int cost;
+        int? seller_id;
+        int? cost;
         string name;
 
         public Article(int id, Repository parent_repository) : base(id, parent_repository)
         {
         }
 
-        protected Article(int id, int Seller_id, int cost, string name, Repository parentRepository) : base (id, parentRepository)
+        protected Article(int id, int? Seller_id, int? Cost, string name, Repository parentRepository) : base (id, parentRepository)
         {
-            this.seller_id = Seller_id;
-            this.cost = cost;
+            this.Seller_id = Seller_id;
+            this.Cost = Cost;
             this.name = name;
         }
 
-        public int Seller_id { get => seller_id; set => seller_id = value; }
-        public int Cost { get => cost; set => cost = value; }
         public string Name { get => name; set => name = value; }
+        public int? Seller_id { get => seller_id; set => seller_id = value; }
+        public int? Cost { get => cost; set => cost = value; }
 
         public static Article[] fromReader(SqlDataReader reader)
         {
@@ -31,14 +31,22 @@ namespace Csharp_ORM_Example
 
             while (reader.Read())
             {
+                int? sellerId = null;
+                if (!reader.IsDBNull(1))
+                    sellerId = reader.GetInt32(1);
+
+                int? cost = null;
+                if (!reader.IsDBNull(2))
+                    cost = reader.GetInt32(2);
+
                 string name = null;
                 if (!reader.IsDBNull(3))
                     name = reader.GetString(3);
 
                 Article a = new Article(
                     reader.GetInt32(0),
-                    reader.GetInt32(1),
-                    reader.GetInt32(2),
+                    sellerId,
+                    cost,
                     name,
                     ArticlesRepository.getInstance()
                     );
